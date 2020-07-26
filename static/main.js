b39c6e6450e3f78d7d9e85a68827f73e92db2938
willhitman <h180128h@hit.ac.zw>
2020-07-26 19:15:54 +0200

    ...

--- static/main.js
+++ static/main.js
@@ -1,10 +1,27 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-links li');
+    //toogle navigation
 
 
     burger.addEventListener('click',()=>{
         nav.classList.toggle('nav-active');
+
+            //animate links
+        navLinks.forEach((link,index)=>{
+            if (link.style.animation){
+                link.style.animation='';
+            }else{
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index/7 +0.2}s`
+            }
+            console.log(index)
+        });
+        //burger animation
+        burger.classList.toggle('toggle');
+
     });
+
+
 }
-navSlide();
+navSlide(); 
